7447e27f5dafc85dd2318c7464435b99596c62a7
neronix17 <39186138+neronix17@users.noreply.github.com>
2021-07-14 03:53:19 +0100

    1.3

---
+++ 1.3/Source/O21Mechadroids/MechadroidsDefOf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace O21Mechadroids
+{
+    [DefOf]
+    public static class MechadroidsDefOf
+    {
+        static MechadroidsDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(MechadroidsDefOf));
+        }
+
+        public static ThingDef O21_MechanoidPowerCell;
+        public static ThingDef O21_MechanoidParts;
+    }
+}
---
+++ 1.3/Source/O21Mechadroids/MechadroidsMod.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace O21Mechadroids
+{
+    public class MechadroidsMod : Mod
+    {
+        public MechadroidsSettings settings;
+        public static MechadroidsMod mod;
+
+        public MechadroidsMod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<MechadroidsSettings>();
+            mod = this;
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+
+            listingStandard.Label("Faction Settings - Changing any of these requires a restart to take effect.");
+            listingStandard.CheckboxLabeled("Civil Mechadroids", ref settings.faction_mechadroidsCivil, "Controls spawning of the Neutral Mechadroid factions.");
+            listingStandard.CheckboxLabeled("Rough Mechadroids", ref settings.faction_mechadroidsRough, "Controls spawning of the Hostile Mechadroid factions.");
+
+            listingStandard.GapLine();
+
+            listingStandard.Label("Mechanoid/Mechanoid Drop Rates");
+            listingStandard.Label("These values are the base values used to determine how much any given mechanoid or mechadroid drops. Changing these will change the intended balance, I'm not going to stop you (I'm obviously helping at this point), but if you have ANY issues from this it's your own fault.");
+            string partsBufferString = settings.baseSpawnRateParts.ToString();
+            listingStandard.TextFieldNumericLabeled("Mechanoid Parts", ref settings.baseSpawnRateParts, ref partsBufferString, 0, 500);
+            string cellsBufferString = settings.baseSpawnRateCells.ToString();
+            listingStandard.TextFieldNumericLabeled("Power Cells", ref settings.baseSpawnRateCells, ref cellsBufferString, 0, 500);
+            string componentsBufferString = settings.baseSpawnRateComponents.ToString();
+            listingStandard.TextFieldNumericLabeled("Advanced Components", ref settings.baseSpawnRateComponents, ref componentsBufferString, 0, 500);
+            string personaBufferString = settings.baseSpawnChancePersonaCore.ToString();
+            listingStandard.TextFieldNumericLabeled("Percentage Chance of an AI Persona Core", ref settings.baseSpawnChancePersonaCore, ref personaBufferString, 0, 100);
+
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Mechadroids";
+        }
+    }
+
+    public class MechadroidsSettings : ModSettings
+    {
+        public bool faction_mechadroidsCivil = true;
+        public bool faction_mechadroidsRough = true;
+
+        public int baseSpawnRateParts = 12;
+        public int baseSpawnRateCells = 4;
+        public int baseSpawnRateComponents = 1;
+        public int baseSpawnChancePersonaCore = 7;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref baseSpawnRateParts, "baseSpawnRateParts", 12);
+            Scribe_Values.Look(ref baseSpawnRateCells, "baseSpawnRateCells", 4);
+            Scribe_Values.Look(ref baseSpawnRateComponents, "baseSpawnRateComponents", 1);
+            Scribe_Values.Look(ref baseSpawnChancePersonaCore, "baseSpawnChancePersonaCore", 7);
+            Scribe_Values.Look(ref faction_mechadroidsCivil, "faction_mechadroidsCivil", true);
+            Scribe_Values.Look(ref faction_mechadroidsRough, "faction_mechadroidsRough", true);
+        }
+    }
+}
